6602578db5a6ff838cf795cd3112e28f9bc54a77
techableteam <techableteam@gmail.com>
2025-01-29 21:06:13 -0800

    fix the signup part and database

--- app/routes/auth.route.js
+++ app/routes/auth.route.js
@@ -9,7 +9,7 @@ router.post(
   [
     body('name').notEmpty().withMessage('Name is required'),
     body('email').isEmail().withMessage('Invalid email'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+    // body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
     body('role').notEmpty().withMessage('Role is required'),
   ],
   (req, res, next) => {
